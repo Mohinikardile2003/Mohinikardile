251aad8525dec4487f363c6a0a75146fddd8dcf4
Kardile Mohini <Kardile Mohini@LAPTOP-FEKQMGMR>
2025-02-12 10:11:03 +0530

    Day One

---
+++ DayOne/src/com/tnsf/dayone/firstprogram/firstperogram.java
@@ -0,0 +1,15 @@
+package com.tnsf.dayone.firstprogram;
+
+public class firstperogram {
+
+	
+	public static void main(String[] args) {
+		//main method
+		//System.out.println("Hello All");//syso+space+enter
+		char ch ='\u00A7';//unicode
+		System.out.println(ch);
+		// asci code 
+		int a='A';
+		System.out.println(a);
+	} 
+}
